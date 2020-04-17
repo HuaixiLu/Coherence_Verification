0b2ad3f50d23b3c6409c2c57f4e028dc3f8f97b6
Huaixi Lu <51335367+HuaixiLu@users.noreply.github.com>
2020-04-16 20:11:59 -0400

    update

--- verification/ccp_eq_check.py
+++ verification/ccp_eq_check.py
@@ -41,7 +41,7 @@ for i in range (CORE_NUMBER):
     else:
         f1.write("(ccp_fsm.pcache_mem.l15_%d.core_tag == ccp_ila.pcache_mem.l15_%d.core_tag) && " %(i,i))
 
-f1.write("assert -name same_msg1 {ccp_fsm.msg1_type == ccp_ila.msg1_type}\n")
-f1.write("assert -name same_msg2 {ccp_fsm.msg2_type == ccp_ila.msg2_type}\n")
-f1.write("assert -name same_msg3 {ccp_fsm.msg3_type == ccp_ila.msg3_type}\n")
+f1.write("assert -name same_msg1 {ccp_fsm.msg1_type == ccp_ila.msg1_type && ccp_fsm.msg1_tag == ccp_ila.msg1_tag && ccp_fsm.msg1_source == ccp_ila.msg1_source}\n")
+f1.write("assert -name same_msg2 {ccp_fsm.msg2_type == ccp_ila.msg2_type && ccp_fsm.msg2_tag == ccp_ila.msg2_tag && ccp_fsm.msg2_source == ccp_ila.msg2_source}\n")
+f1.write("assert -name same_msg3 {ccp_fsm.msg3_type == ccp_ila.msg3_type && ccp_fsm.msg3_source == ccp_ila.msg3_source && (ccp_fsm.msg3_type != `MSG_TYPE_LOAD_MEM_ACK || ccp_fsm.msg3_tag == ccp_ila.msg3_tag)}\n")
 f1.write("assert -name same_cache {ccp_fsm.l2.cache_state == ccp_ila.l2.cache_state && ccp_fsm.l2.cache_data == ccp_ila.l2.cache_data && ccp_fsm.l2.cache_tag == ccp_ila.l2.cache_tag}\n")
